390736f67fe4971b328d99c4b42934ddc52ffb17
林锦骏 <673115816@qq.com>
2021-09-07 18:06:13 +0800

    normal route

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 const app = require('express')();
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+
+app.get('/', (req, res) => {
+    res.send('hello rtc')
+})
+
+const io = require('socket.io')(server, { cors: true });
 
 const hash = new Map
 
 io.on('connection', (socket) => { 
+    
     socket.on('create', (config) => {
         const {uid} = config
         socket.join(uid)
         hash.set(uid, config)
-        socket.emit('create-success')
+        socket.emit('create-success', '创建成功回调')
     })    
 
     socket.on('join', (data) => {
         const {uid} = data
         socket.join(uid)
-        socket.emit('join-success')
+        socket.emit('join-success', '加入成功回调')
     })
 
     socket.on('close', (config) => {
@@ -24,8 +30,13 @@ io.on('connection', (socket) => {
         socket.to(uid).emit('close')
     })
 
-    socket.on('disconnection', () => {
-        
+    socket.on('disconnecting', () => {
+        console.log(socket.rooms); // the Set contains at least the socket ID
+    });
+
+
+    socket.on('disconnect', () => {
+        socket.emit('disconnect-success', '断开成功回调')
     })
  });
-server.listen(3000);
+server.listen(3333);
