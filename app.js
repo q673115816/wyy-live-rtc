a700f5c91626e1a28cfe1e9369b1f9a82fe71d36
鸽蛋白 <673115816@qq.com>
2021-09-08 00:29:53 +0800

    midware

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const app = require('express')();
+const helmet = require('helmet')
+const cors = require('cors')
+app.use(cors())
+app.use(helmet())
+
 const server = require('http').createServer(app);
 
 const io = require('socket.io')(server, { cors: true });
