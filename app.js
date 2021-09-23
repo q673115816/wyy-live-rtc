35ec06bb043be91eb8269de8bfa0c1ddb1550c4a
林锦骏 <673115816@qq.com>
2021-09-23 16:55:17 +0800

    remove sentry

--- app.js
+++ app.js
@@ -42,59 +42,6 @@ app.get('/list', (req, res) => {
     })
 })
 
-app.post('/sentry', (req, res) => {
-    console.log('进入sentry')
-    // res.json(util.inspect(req.body))
-    // console.log(req.body)
-    // res.send(req.body)
-
-    const options = {
-        host: 'https://oapi.dingtalk.com',
-        port: 443,
-        path: '/robot/send?access_token=7b357929ac3f5950a3bbc2483b467a5dd6b8476b6e1593097d336204b625475a',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }
-
-    const postData = JSON.stringify({
-        "at": {
-            "atMobiles": [
-                "180xxxxxx"
-            ],
-            "atUserIds": [
-                "章忠善"
-            ],
-            "isAtAll": true
-        },
-        "text": {
-            "content": "网易云线上报错"
-        },
-        "msgtype": "text"
-    })
-
-    const request = https.request(options, (res) => {
-        console.log(`STATUS: ${res.statusCode}`);
-        console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-        res.setEncoding('utf8');
-        res.on('data', (chunk) => {
-            console.log(`BODY: ${chunk}`);
-        });
-        res.on('end', () => {
-            console.log('No more data in response.');
-        });
-    });
-
-    request.on('error', (e) => {
-        console.error(`problem with request: ${e.message}`);
-    });
-
-    // Write data to request body
-    request.write(postData);
-    request.end();
-})
-
 io.on('connection', (socket) => { 
     
     socket.on('create', (config) => {
