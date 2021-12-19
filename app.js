fa333acdff54ab79d6aa143d00796a7f99c773e8
林锦骏 <673115816@qq.com>
2021-12-20 00:16:11 +0800

    mongoose

--- app.js
+++ app.js
@@ -3,56 +3,50 @@ const helmet = require('helmet')
 const cors = require('cors')
 const app = express();
 app
-.use(cors())
-.use(helmet())
-.use(express.urlencoded({extended: true}))
+    .use(cors())
+    .use(helmet())
+    .use(express.urlencoded({ extended: true }))
     .use(express.json({ type: 'application/json' }))
 const server = require('http').createServer(app);
-require('./services')
+const { liveSchema, liveModel } = require('./services')
 const io = require('socket.io')(server, { cors: true });
-
-const hash = new Map
-
-hash.set('7', {
-    "password": "010110",
-    "title": "测试房",
-    "user": "admin",
-    "viewer": 10,
-    "banner": "https://gimg2.baidu.com/image_search/src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20181226%2Ffab5d53db33e451aa77bd0e2b29eb1fa.jpeg&refer=http%3A%2F%2F5b0988e595225.cdn.sohucs.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1633623474&t=95f0190c1c24be3e3a2e6c1e809d2b61"
-})
 
 app.get('/', (req, res) => {
     res.send('hello rtc')
 })
 
-app.get('/count', (req, res) => {
+app.get('/count', async (req, res) => {
+    const data = await liveModel.count({})
     res.json({
         code: 200,
-        data: hash.size
+        data
     })
 })
 
-app.get('/list', (req, res) => {
+app.get('/list', async (req, res) => {
+    const data = await liveModel.find({}, { _id: false, __v: false })
+    console.log(data)
     res.json({
         code: 200,
-        data: [...hash.values()]
+        data
     })
 })
 
-io.on('connection', (socket) => { 
-    
-    socket.on('create', (config) => {
-        const {uid} = config
+io.on('connection', (socket) => {
+
+    socket.on('create', async (config) => {
+        const { uid } = config
         socket.join(uid)
-        hash.set(uid, config)
+        const newLiveModel = new liveModel(config)
+        await newLiveModel.save()
         socket.emit('create-success', '创建成功回调')
-    })    
+    })
 
     socket.on('join', async (data) => {
-        const {uid} = data
-        const room = hash.get(uid)
-        if(!room) return socket.emit('join-error', '房间不存在')
-        const {viewer} = room
+        const { uid } = data
+        const room = await liveModel.findOne({ uid })
+        if (!room) return socket.emit('join-error', '房间不存在')
+        const { viewer } = room
         const users = await io.in(room).allSockets()
         const roomCount = users.size
         if (roomCount >= viewer) return socket.emit('join-error', '房间满员')
@@ -61,16 +55,17 @@ io.on('connection', (socket) => {
     })
 
     socket.on('leave', (data) => {
-        const {uid} = data
+        const { uid } = data
         socket
             .leave(uid)
         socket
             .emit('message', '离开房间')
     })
 
-    socket.on('close', (config) => {
-        const {uid} = config
-        if(!hash.has(uid)) return
+    socket.on('close', async (config) => {
+        const { uid } = config
+        await liveModel.findOneAndDelete({ uid })
+        // if (!room) return
         socket.to(uid).emit('close')
     })
 
@@ -82,7 +77,7 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         socket.emit('disconnect-success', '断开成功回调')
     })
- });
+});
 
 const PORT = process.env.PORT || 3333
 
--- services.js
+++ services.js
@@ -2,17 +2,39 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 
 mongoose.connect(process.env.MONGODB_URI);
-const { Schema } = mongoose
+const { Schema, model } = mongoose
 const liveSchema = new Schema({
     password: String,
     title: String,
+    uid: String,
     user: String,
     viewer: Number,
     banner: String,
 });
 
-const liveModel = mongoose.model('live', liveSchema)
+const liveModel = model('Live', liveSchema)
 
-liveModel.find().exec((err, live) => {
-    console.log('live', live);
-})
+// liveModel.count({}).then(res => console.log(res))
+
+module.exports = {
+    liveModel, liveSchema
+}
+// const admin = new liveModel({
+//     "password": "010110",
+//     "title": "测试房",
+//     "user": "admin",
+//     "viewer": 10,
+//     "banner": "https://gimg2.baidu.com/image_search/src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20181226%2Ffab5d53db33e451aa77bd0e2b29eb1fa.jpeg&refer=http%3A%2F%2F5b0988e595225.cdn.sohucs.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1633623474&t=95f0190c1c24be3e3a2e6c1e809d2b61"
+// })
+// admin.save().then((res) => console.log(res))
+
+// liveModel.find().then((res) => console.log(res))
+
+// const CatSchema = new Schema({
+//     name: String
+// })
+
+// const CatModel = model('Cat', CatSchema)
+
+// const kitty = new CatModel({name: 'zibenjia'})
+// CatModel.find().then((res) => console.log(res))
