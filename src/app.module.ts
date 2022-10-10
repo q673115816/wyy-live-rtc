7c3774736ea5cffd312e5081b8d593984342e147
林锦骏 <673115816@qq.com>
2022-10-10 18:08:49 +0800

    save

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { DirectiveLocation, GraphQLDirective } from 'graphql';
+import {join} from 'path'
+import { upperDirectiveTransformer } from './common/directives/upper-case.directive';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
@@ -12,6 +17,20 @@ import { CatsModule } from './cats/cats.module';
     // ConfigModule.forRoot({
     //   isGlobal: true,
     //   envFilePath: ['.env.development.local', '.env.development'],
+    // }),
+    // GraphQLModule.forRoot<ApolloDriverConfig>({
+    //   driver: ApolloDriver,
+    //   autoSchemaFile: 'schema.gql',
+    //   transformSchema: (schema) => upperDirectiveTransformer(schema, 'upper'),
+    //   installSubscriptionHandlers: true,
+    //   buildSchemaOptions: {
+    //     directives: [
+    //       new GraphQLDirective({
+    //         name: 'upper',
+    //         locations: [DirectiveLocation.FIELD_DEFINITION],
+    //       }),
+    //     ],
+    //   },
     // }),
     UserModule,
     CatsModule,
